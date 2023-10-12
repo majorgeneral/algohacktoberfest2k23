465b122382d4c8677a0098c5c3f8f50d8bcf1244
Narayan Sharma <117571068+85Narayan@users.noreply.github.com>
2023-10-12 22:42:52 +0530

    Create linkedlist.cpp

---
+++ linkedlist.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+// Node class
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int value) {
+        data = value;
+        next = nullptr;
+    }
+};
+
+// Linked list class
+class LinkedList {
+private:
+    Node* head;
+
+public:
+    LinkedList() {
+        head = nullptr;
+    }
+
+    // Insert a new node at the beginning of the list
+    void insert(int value) {
+        Node* newNode = new Node(value);
+        if (head == nullptr) {
+            head = newNode;
+        } else {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+
+    // Display the elements of the linked list
+    void display() {
+        Node* temp = head;
+        while (temp != nullptr) {
+            std::cout << temp->data << " ";
+            temp = temp->next;
+        }
+        std::cout << std::endl;
+    }
+};
+
+int main() {
+    LinkedList myList;
+
+    // Inserting elements into the linked list
+    myList.insert(5);
+    myList.insert(10);
+    myList.insert(15);
+    myList.insert(20);
+
+    // Displaying the linked list
+    myList.display();
+
+    return 0;
+}
